616ae37e88b9719f4626aca933a1659098111ee6
Ryan Edward Lima <Ryan.Lima@nau.edu>
2021-02-05 14:54:20 -0700

    changes to numbering of files

--- data-raw/sandbarSitesTb.R
+++
@@ -1,8 +0,0 @@
-## code to prepare `SandbarSitesTb` dataset goes here
-
-sandbarSitesDf <- read.csv("data-raw/sandbar_sites_df.csv", header = T)
-sandbarSitesTb <- as_tibble(sandbarSitesDf)
-
-
-
-usethis::use_data(sandbarSitesTb, overwrite = TRUE)
